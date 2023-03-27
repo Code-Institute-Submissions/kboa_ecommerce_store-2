ecd7bcede3c269f24403a92482fc247ebcfba62c
kiksgold <kiksgold@gmail.com>
2023-03-27 15:10:37 +0000

    Created checkout templates, static and urls

--- kboa_foods/settings.py
+++ kboa_foods/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'checkout',
 
     # Other
+    'crispy_forms',
     'storages',
     'djmoney',
 ]
@@ -67,6 +68,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'kboa_foods.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -84,6 +87,10 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'cart.contexts.cart_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
