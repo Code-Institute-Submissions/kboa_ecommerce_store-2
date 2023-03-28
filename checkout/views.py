c1955773e6b97ec1495ad4284de38c1d072f8440
kiksgold <kiksgold@gmail.com>
2023-03-28 10:08:37 +0000

    Added stripe_elements.js

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51MW06wDHLVYaCninCs35YJ7ttM3yqrJbnVuy4XpEUMUuj7djWGUEet2KjzYMsbOx8Nlds8jkQeyD0W1X08bgFxQZ00vSWQcWLw',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
