3e895e8f592b256ce6e6fbad6ec7027b436f2b7b
kiksgold <kiksgold@gmail.com>
2023-04-04 15:19:06 +0000

    Added views, template and urls to wishlist app

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def view_wishlist(request):
+    """ A view to return the wishlist/saved items """
+
+    return render(request, 'wishlist/wishlist.html')
