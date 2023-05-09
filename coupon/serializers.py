d59efcb301038251c21f2bd3433cc953ae22065e
kiksgold <kiksgold@gmail.com>
2023-05-09 12:24:50 +0000

    Add models and views to coupon app

---
+++ coupon/serializers.py
@@ -0,0 +1,7 @@
+from django.core import serializers
+from .models import Coupon
+
+class CouponSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Coupon
+        fields = '__all__'
