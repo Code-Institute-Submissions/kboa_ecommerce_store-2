41b157c991ad41ee5c9bd942a004019e6b6d7e11
kiksgold <kiksgold@gmail.com>
2023-05-17 20:58:54 +0000

    create models.py in wishlish

--- wishlist/models.py
+++ wishlist/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from products.models import Product
+from profiles.models import UserProfile
 
-# Create your models here.
+
+class Wishlist(models.Model):
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
+                                     null=True, blank=True, related_name='wishlist')
+    product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
+    added_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.item
