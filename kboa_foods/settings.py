25dbd776e8c1ecd8f76d018061c3330f02dfb4fc
kiksgold <kiksgold@gmail.com>
2023-05-22 21:06:33 +0000

    Edit settings.py and deployment

--- kboa_foods/settings.py
+++ kboa_foods/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['kboa-foods.herokuapp.com', 'localhost', '8000-kiksgold-kboaecommerces-rp8u505k8h8.ws-eu97.gitpod.io']
+ALLOWED_HOSTS = ['kboa-foods.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -114,7 +114,7 @@ SITE_ID = 1
 
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
-ACCOUNT_EMAIL_VERIFICATION = 'none'
+ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
 ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
 ACCOUNT_USERNAME_MIN_LENGTH = 4
 LOGIN_URL = '/accounts/login/'
